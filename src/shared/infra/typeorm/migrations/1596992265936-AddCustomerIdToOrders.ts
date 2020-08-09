97bc025589a08ae512a6992cbe3543e671d8f3bd
Manoel Dantas <manoel.lobo@gmail.com>
2020-08-09 14:16:18 -0300

    migration: foreign keys to order and orderProducts

---
+++ src/shared/infra/typeorm/migrations/1596992265936-AddCustomerIdToOrders.ts
@@ -0,0 +1,33 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddCustomerIdToOrders1596992265936
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders',
+      new TableColumn({ name: 'customer_id', type: 'uuid', isNullable: true }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders',
+      new TableForeignKey({
+        name: 'OrderCustomer',
+        columnNames: ['customer_id'],
+        referencedTableName: 'customers',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders', 'OrderCustomer');
+
+    await queryRunner.dropColumn('orders', 'customer_id');
+  }
+}
